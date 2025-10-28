bea7ddc5d1195e85c23ac92220b6c39c46cfc62a
zabronax <zabronax@gmail.com>
2025-10-28 10:37:35 +0100

    evaluate query

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 ﻿List<int> integers = new List<int> { 1, 2, 3, 4, 5, 6 };
 
-// newCollection != List, det er en spørring, som ikke er evaluert enda
+// newCollection != List, det er en spørring
 var newCollection = integers
                     // Filtrer ut all partall
                     .Where(n => n % 2 == 0)
                     // Multipliser de med 2
                     .Select(n => n * 2)
                     // Så filtrer ut all som er større enn 4
-                    .Where(n => n > 4);
+                    .Where(n => n > 4)
+                    // Evaluer spørringen
+                    .ToList();
 
 integers.Add(14);
 
