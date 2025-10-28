e92042b9f0e1222630037300b504a4003b7dd5c6
zabronax <zabronax@gmail.com>
2025-10-28 09:38:07 +0100

    refactor: convert to LINQ

--- Program.cs
+++ Program.cs
@@ -1,15 +1,8 @@
 ﻿List<int> integers = new List<int> { 1, 2, 3, 4, 5, 6 };
 
-List<int> evens = new List<int>();
-
-// Even = Partall / Evens = Flere partall
-foreach (int entry in integers)
-{
-  if (entry % 2 == 0)
-  {
-    evens.Add(entry);
-  }
-}
+var evens = from n in integers
+            where n % 2 == 0
+            select n;
 
 Console.WriteLine("Here are all even numbers");
 foreach (int number in evens)
