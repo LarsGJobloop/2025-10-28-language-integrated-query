f952a569ef861be735b46ca289b5f711e555913a
zabronax <zabronax@gmail.com>
2025-10-28 10:26:10 +0100

    feat: add method chaining

--- Program.cs
+++ Program.cs
@@ -1,20 +1,16 @@
 ﻿List<int> integers = new List<int> { 1, 2, 3, 4, 5, 6 };
 
-// First LINQ variation
-var evens = from n in integers
-            where n % 2 == 0
-            select n;
+var newCollection = integers
+                    // Filtrer ut all partall
+                    .Where(n => n % 2 == 0)
+                    // Multipliser de med 2
+                    .Select(n => n * 2)
+                    // Så filtrer ut all som er større enn 4
+                    .Where(n => n > 4);
 
-var odds = integers
-              .Where(n => n % 2 == 1);
 
-Console.WriteLine("Here are all even numbers for 'evens'");
-foreach (int number in evens)
-{
-  Console.WriteLine(number);
-}
-Console.WriteLine("Here are all odd numbers for 'odds");
-foreach (int number in odds)
+Console.WriteLine("The new collection");
+foreach (int number in newCollection)
 {
   Console.WriteLine(number);
 }
