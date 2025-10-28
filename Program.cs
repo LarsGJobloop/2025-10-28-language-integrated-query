4000b03d8ad70f7026804df154daa9c0c4ccf54e
zabronax <zabronax@gmail.com>
2025-10-28 09:51:07 +0100

    feat: add method syntax

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
 ﻿List<int> integers = new List<int> { 1, 2, 3, 4, 5, 6 };
 
+// First LINQ variation
 var evens = from n in integers
             where n % 2 == 0
             select n;
 
-Console.WriteLine("Here are all even numbers");
+var odds = integers
+              .Where(n => n % 2 == 1);
+
+Console.WriteLine("Here are all even numbers for 'evens'");
 foreach (int number in evens)
+{
+  Console.WriteLine(number);
+}
+Console.WriteLine("Here are all odd numbers for 'odds");
+foreach (int number in odds)
 {
   Console.WriteLine(number);
 }
