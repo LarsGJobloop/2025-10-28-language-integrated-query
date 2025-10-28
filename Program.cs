1375fc98bba11f84f118c9a3b51d71af10e5fa5d
zabronax <zabronax@gmail.com>
2025-10-28 09:32:47 +0100

    feat: setup loop for even number extraction

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> integers = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+List<int> evens = new List<int>();
+
+// Even = Partall / Evens = Flere partall
+foreach (int entry in integers)
+{
+  if (entry % 2 == 0)
+  {
+    evens.Add(entry);
+  }
+}
+
+Console.WriteLine("Here are all even numbers");
+foreach (int number in evens)
+{
+  Console.WriteLine(number);
+}
